e507fd6448410dfb11e0dbcb1d3a6de97428ada2
CSC120shahe <hesha0709@gmail.com>
2025-09-24 01:45:51 -0400

    add doc files such as SQLite Cheatsheet,frontendAPIUsageGuide

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from app import create_app,db
+from flask_migrate import Migrate
 
 app = create_app()
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all() # make database initiation
     app.run(debug=True)
+
+# debug conveniently locally, you can manually open the browser to access it in the command line:
+# open http://127.0.0.1:5000   # macOS
+# start http://127.0.0.1:5000  # Windows
