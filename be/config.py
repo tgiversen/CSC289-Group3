b8c92c0f02e1556e89c25424ba690c975c06afe3
Tyler Khin <145476492+your-extended-car-warranty@users.noreply.github.com>
2025-10-05 13:52:32 -0400

    Update config.py format

--- be/config.py
+++ be/config.py
@@ -9,9 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev_secret_key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
-        basedir, "instance", "site.db"
-    )
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "instance", "site.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
