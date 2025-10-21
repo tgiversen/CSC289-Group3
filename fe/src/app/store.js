ca72898a7f7f176eb4e9cc8a3f1e713a1fd9ef15
shamilton7 <130393252+shamilton7@users.noreply.github.com>
2025-10-21 07:18:09 -0400

    Update store.js
    Added regReducer

--- fe/src/app/store.js
+++ fe/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../stores/authSlice";
 import gameReducer from "../stores/gameSlice";
+import regReducer from "..stores/regSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    reg: regReducer,
     game: gameReducer,
   },
 });
