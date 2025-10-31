4c5b00a856fa99ce816dd21583ba537d5602ab3d
Tyler Khin <khinspirator@gmail.com>
2025-10-31 00:14:00 -0400

    removed old test file structure

--- tests/test_models.py
+++
@@ -1,4 +0,0 @@
-# Tyler Khin
-# CSC289 - Group 9
-# September 19th, 2025
-# test_models.py: Unit tests for database models
--- tests/test_utils.py
+++
@@ -1,4 +0,0 @@
-# Tyler Khin
-# CSC289 - Group 9
-# September 19th, 2025
-# test_utils.py: Unit/Integration tests for helper functions
