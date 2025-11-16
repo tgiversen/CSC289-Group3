b65c5363c802eda2a64eb22a1adca6752f762acd
CSC120shahe <hesha0709@gmail.com>
2025-11-16 10:28:05 -0500

    modify the API_BASE_URL to fix the cors

--- fe/src/stores/regSlice.jsx
+++ fe/src/stores/regSlice.jsx
@@ -1,7 +1,9 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+// const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "http://127.0.0.1:5000/api";
+
 const initialState = { user: null, status: "idle", error: null };
 
 export const register = createAsyncThunk(
